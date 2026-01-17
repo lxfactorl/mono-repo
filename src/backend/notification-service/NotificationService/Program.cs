f0fe49675e26bc62174c4a4c9762a05ee887df5a
lxfactorl <fiodorov@gmail.com>
2026-01-17 17:07:16 +0200

    test: introduce warning to verify strict build gate

--- src/backend/notification-service/NotificationService/Program.cs
+++ src/backend/notification-service/NotificationService/Program.cs
@@ -2,8 +2,9 @@ using Microsoft.AspNetCore.Builder;
 using NotificationService.Bootstrap;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.ConfigureServices();
+builder.ConfigureEndpoints();
+int unusedVariable = 42;
 
 var app = builder.Build();
 
